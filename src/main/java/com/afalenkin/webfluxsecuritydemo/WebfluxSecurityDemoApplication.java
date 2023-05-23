c77bb4d096dfafa5a569e83d65ef2ce2827bdc28
AlenkinAndrew <oxqq@ya.ru>
2023-05-23 21:48:32 +0300

    Initial commit

---
+++ src/main/java/com/afalenkin/webfluxsecuritydemo/WebfluxSecurityDemoApplication.java
@@ -0,0 +1,13 @@
+package com.afalenkin.webfluxsecuritydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxSecurityDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebfluxSecurityDemoApplication.class, args);
+	}
+
+}
